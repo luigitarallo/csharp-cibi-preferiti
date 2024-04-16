124f7690d6ed3f1d43bd7a5d42436cab852d17ec
gigitarallo <luigi.tarallo@thetruefans.com>
2024-04-16 16:33:51 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp_cibi_preferiti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "gelato", "bolognese", "cioccolato", "timballo pasta","caffè" };
+            Console.WriteLine("Cibi preferiti: " + cibiPreferiti.Length + " ");
+            for (var i = 0; i < cibiPreferiti.Length; i++)
+            {
+                Console.WriteLine(i + 1 + " " + cibiPreferiti[i] + " ");
+            }
+            Console.WriteLine("Preferito in assoluto: " + cibiPreferiti[0]);
+            Console.WriteLine("Meno preferito: " + cibiPreferiti[7]);
+        }
+    }
+}
